ff06b2323e8a1c320019679f0e25f5c21b1a0a78
felixkurnia7 <72884846+felixkurnia7@users.noreply.github.com>
2026-06-24 14:58:28 +0700

    Setup environment scene forest

--- Assets/Resources/Fungus/Scripts/Editor/FlowchartWindow.cs
+++ Assets/Resources/Fungus/Scripts/Editor/FlowchartWindow.cs
@@ -344,7 +344,8 @@ namespace Fungus.EditorUtils
         {
             //an undo redo may have added or removed blocks so
             UpdateBlockCollection();
-            flowchart.UpdateSelectedCache();
+            if (flowchart != null)
+                flowchart.UpdateSelectedCache();
             Repaint();
         }
 
